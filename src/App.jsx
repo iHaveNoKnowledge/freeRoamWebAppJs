d243d5c1d5433423c70911b073e3036e4beb966e
iHaveNoKnowledge <hackdk12@gmail.com>
2024-07-16 12:05:56 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
         <TableDocument />
       </PDFViewer> */}
       <FormikLoginForm />
-      <div>แมว</div>
+      {/* <div>แมว</div> */}
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "fade-bottom":
+          "linear-gradient(to bottom, rgba(0,0,0,0.0) 0%, rgba(200,200,220,0.99) 70%), url('https://wallpapers.com/images/hd/windows-xp-wallpaper-dump-ovzbab2skq4ft6jl.jpg') ",
+      },
+    },
   },
   plugins: [],
 };
